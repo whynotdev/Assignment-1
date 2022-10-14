2547405ff6b2ea3be85f38824deab56cdb9fc5b2
whynotdev <115531149+whynotdev@users.noreply.github.com>
2022-10-14 15:32:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var title = document.getElementById('title')
+    var num1=prompt("Enter First number")
+    var num2=prompt("Enter Second number")
+    
+         if (num1 > num2 )
+             {
+              
+                alert(num1 + " is larger than " + num2)
+            }
+             
+             else
+                {
+                     alert(num2 + " is larger than " + num1)
+                 }
+                
